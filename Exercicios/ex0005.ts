37057f3fb456b182d3395eef48ca97a916643ef8
Leandro Costa <lcostasantos1987@gmail.com>
2022-07-14 02:13:09 -0300

    update
    update

---
+++ Exercicios/ex0005.ts
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    dance() {
+        console.log(this.name + " is dancing");
+    }
+}
+var brad = new Person("Brad");
+brad.dance();
+class AwesomePerson extends Person {
+    dance() {
+        console.log("Awesome");
+        super.dance();
+    }
+}
+var rob = new AwesomePerson("Robb");
